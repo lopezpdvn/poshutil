c532c87680841c58f8a7ab48182ca7fcb22656c0
Pedro I. López <dreilopz@gmail.com>
2014-08-03 17:59:38 -0500

    get_named_dir Function.

--- functions.sh
+++ functions.sh
@@ -15,3 +15,16 @@ is_older() {
     test "$(find "$dirp" -maxdepth 1 -mmin +$age_threshold -name "$fp")"
     return $?
 }
+
+get_named_dir() {
+    if [ -z $1 ]
+    then
+        return 1
+    fi
+    fp="$1"
+    maxdepth=${2-'1'}
+    dirp=${3-$(pwd)}
+
+    find "$dirp" -maxdepth "$maxdepth" -type d -name "$fp" 2>/dev/null |
+        sort | head -n 1
+}
